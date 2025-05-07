252526dc3fd8f3495d48fde94b8ab1f33138a1bd
Aleksey Larkov <djalekseeff@gmail.com>
2025-05-07 23:06:24 +0300

    Начали Задача 1.1.4 файл hibernate.cfg.xml

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.config.DataBaseConfig;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,5 +19,12 @@ public class Util {
                 DataBaseConfig.USERNAME,
                 DataBaseConfig.PASSWORD);
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return new Configuration()
+                .configure()
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
     }
 }
