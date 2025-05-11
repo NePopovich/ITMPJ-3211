06e1e6c9e175bddba021321bb8a2b6e7427c9195
Aleksey Larkov <djalekseeff@gmail.com>
2025-05-11 22:37:38 +0300

    Исправлен Main, добавлен файл .properties

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,20 +1,44 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger logger = Logger.getLogger("Task");
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-        UserService userService = new UserServiceImpl();
+
+        UserService userService = getUserService();
         userService.createUsersTable();
-        userService.saveUser("Alesha", "Larkov", (byte) 30);
-        userService.saveUser("Ivan", "Ivanov", (byte) 20);
-        userService.saveUser("Dima", "Malinov", (byte) 25);
-        userService.saveUser("Petr", "Petrov", (byte) 45);
-        userService.getAllUsers().forEach(System.out::println);
+
+        for (int i = 0; i < 4; i++) {
+            User user = createUser();
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+        }
+
+        userService.getAllUsers().forEach(e -> logger.info(e.toString()));
         userService.removeUserById(1L);
         userService.cleanUsersTable();
         userService.dropUsersTable();
+    }
+
+    private static UserService getUserService(){
+        return new UserServiceImpl();
+    }
+
+    private static User createUser() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Имя: ");
+        String name = in.nextLine();
+        System.out.print("Фамилия: ");
+        String lastName = in.nextLine();
+        System.out.print("Возраст: ");
+        Byte age = in.nextByte();
+        return new User(name, lastName, age);
     }
 }
